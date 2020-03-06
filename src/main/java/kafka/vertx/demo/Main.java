68a72d032e11e836c4fdd4bd6d6871d88c806171
Julien Ponge <jponge@redhat.com>
2020-03-07 00:18:07 +0100

    A rewrite without service proxies
    Also:
    
    - direct JsonObject rather than POJO mapping,
    - 1 Kakfa consumer per (consumer) websocket which is more correct (1 can stop, while other keep consuming events)

--- src/main/java/kafka/vertx/demo/Main.java
+++ src/main/java/kafka/vertx/demo/Main.java
@@ -1,13 +1,7 @@
 package kafka.vertx.demo;
 
-import io.vertx.core.CompositeFuture;
-import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
+import io.vertx.core.*;
 import io.vertx.core.spi.resolver.ResolverProvider;
-import kafka.vertx.demo.consumer.ConsumerVerticle;
-import kafka.vertx.demo.producer.ProducerVerticle;
-import kafka.vertx.demo.websocket.WebsocketVerticle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,9 +11,9 @@ import static java.lang.System.currentTimeMillis;
 
 public class Main {
 
-  private static String websocketVerticleName = "websocket";
-  private static String producerVerticleName = "producer";
-  private static String consumerVerticleName = "consumer";
+  public static final String TOPIC = "demo";
+  public static final String PERIODIC_PRODUCER_ADDRESS = "demo.periodic.producer";
+  public static final String PERIODIC_PRODUCER_BROADCAST = "demo.periodic.producer.broadcast";
 
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
@@ -28,45 +22,17 @@ public class Main {
 
     // Set vertx timeout to deal with slow DNS connections
     System.setProperty(ResolverProvider.DISABLE_DNS_RESOLVER_PROP_NAME, "true");
+
     Vertx vertx = Vertx.vertx(
       new VertxOptions()
         .setWarningExceptionTime(10).setWarningExceptionTimeUnit(TimeUnit.SECONDS)
         .setMaxEventLoopExecuteTime(20).setMaxEventLoopExecuteTimeUnit((TimeUnit.SECONDS)));
 
-    Future<String> webSocketDeployment = vertx
-      .deployVerticle(new WebsocketVerticle())
-      .onSuccess(id -> verticleDeployed(websocketVerticleName, id))
-      .onFailure(err -> verticleFailedToDeploy(websocketVerticleName, err));
+    Future<String> periodicProducerDeployment = vertx.deployVerticle(new PeriodicProducer());
+    Future<String> webSocketServerDeployment = vertx.deployVerticle(new WebSocketServer());
 
-    Future<String> producerDeployment = vertx
-      .deployVerticle(new ProducerVerticle())
-      .onSuccess(id -> verticleDeployed(producerVerticleName, id))
-      .onFailure(err -> verticleFailedToDeploy(producerVerticleName, err));
-
-    Future<String> consumerDeployment = vertx
-      .deployVerticle(new ConsumerVerticle())
-      .onSuccess(id -> verticleDeployed(consumerVerticleName, id))
-      .onFailure(err -> verticleFailedToDeploy(consumerVerticleName, err));
-
-    // Create CompositeFuture to wait for verticles to start
-    CompositeFuture.join(webSocketDeployment, producerDeployment, consumerDeployment)
-      .onSuccess(res -> appStarted(currentTimeMillis() - startTime))
-      .onFailure(Main::appFailedToStart);
-  }
-
-  private static void appFailedToStart(Throwable t) {
-    logger.error("❌ Application failed to start", t);
-  }
-
-  private static void appStarted(long duration) {
-    logger.info("✅ Application has started in {}ms, go to http://localhost:8080 to see the app running.", duration);
-  }
-
-  private static void verticleDeployed(String name, String id) {
-    logger.info("🚀 Verticle {} deployed: {}", name, id);
-  }
-
-  private static void verticleFailedToDeploy(String name, Throwable err) {
-    logger.error("❌ Verticle {} failed to deploy: {}", name, err.getMessage());
+    CompositeFuture.all(periodicProducerDeployment, webSocketServerDeployment)
+      .onSuccess(ok -> logger.info("✅ Application started in {}ms", currentTimeMillis() - startTime))
+      .onFailure(err -> logger.error("❌ Application failed to start", err));
   }
 }
---
+++ src/main/java/kafka/vertx/demo/WebSocketServer.java
@@ -0,0 +1,125 @@
+package kafka.vertx.demo;
+
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.kafka.client.common.TopicPartition;
+import io.vertx.kafka.client.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+
+public class WebSocketServer extends AbstractVerticle {
+
+  private static final String PRODUCE_PATH = "/demoproduce";
+  private static final String CONSUME_PATH = "/democonsume";
+
+  private static final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
+
+  private HashMap<String, String> kafkaConfig;
+
+  @Override
+  public void start(Promise<Void> startPromise) {
+    loadKafkaConfig(startPromise);
+
+    Router router = Router.router(vertx);
+    router.get().handler(StaticHandler.create());
+
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .webSocketHandler(this::handleWebSocket)
+      .listen(8080, "localhost")
+      .onSuccess(ok -> {
+        logger.info("🚀 WebSocketServer started");
+        startPromise.complete();
+      })
+      .onFailure(err -> {
+        logger.error("❌ WebSocketServer failed to listen", err);
+        startPromise.fail(err);
+      });
+  }
+
+  private void loadKafkaConfig(Promise<Void> startPromise) {
+    ConfigRetriever.create(vertx,
+      new ConfigRetrieverOptions().addStore(
+        new ConfigStoreOptions()
+          .setType("file")
+          .setFormat("properties")
+          .setConfig(new JsonObject().put("path", "kafka.properties").put("raw-data", true))))
+      .getConfig()
+      .onSuccess(config -> {
+        kafkaConfig = new HashMap<>();
+        config.forEach(entry -> kafkaConfig.put(entry.getKey(), entry.getValue().toString()));
+      })
+      .onFailure(startPromise::fail);
+  }
+
+  private void handleWebSocket(ServerWebSocket webSocket) {
+    switch (webSocket.path()) {
+      case PRODUCE_PATH:
+        handleProduceSocket(webSocket);
+        break;
+      case CONSUME_PATH:
+        handleConsumeSocket(webSocket);
+        break;
+      default:
+        webSocket.reject();
+    }
+  }
+
+  private void handleProduceSocket(ServerWebSocket webSocket) {
+    EventBus eventBus = vertx.eventBus();
+
+    webSocket.handler(buffer -> {
+      String action = buffer.toJsonObject().getString("action", "none");
+      if ("start".equals(action) || "stop".equals(action)) {
+        eventBus.send(Main.PERIODIC_PRODUCER_ADDRESS, action);
+      }
+    });
+
+    eventBus.<JsonObject>consumer(Main.PERIODIC_PRODUCER_BROADCAST, message ->
+      eventBus.send(webSocket.textHandlerID(), message.body().encode()));
+  }
+
+  private void handleConsumeSocket(ServerWebSocket webSocket) {
+    KafkaConsumer<String, JsonObject> kafkaConsumer = KafkaConsumer.create(vertx, kafkaConfig);
+
+    kafkaConsumer.exceptionHandler(err -> logger.error("Kafka error", err));
+
+    kafkaConsumer.handler(record -> {
+      JsonObject payload = new JsonObject()
+        .put("topic", record.topic())
+        .put("partition", record.partition())
+        .put("offset", record.offset())
+        .put("timestamp", record.timestamp())
+        .put("value", record.value());
+      vertx.eventBus().send(webSocket.textHandlerID(), payload.encode());
+    });
+
+    kafkaConsumer.subscribe(Main.TOPIC)
+      .onSuccess(ok -> logger.info("Subscribed to {}", Main.TOPIC))
+      .onFailure(err -> logger.error("Could not subscribe to {}", Main.TOPIC, err));
+
+    TopicPartition partition = new TopicPartition().setTopic(Main.TOPIC);
+
+    webSocket.handler(buffer -> {
+      String action = buffer.toJsonObject().getString("action", "none");
+      if ("start".equals(action)) {
+        kafkaConsumer.resume(partition)
+          .onFailure(err -> logger.error("Cannot resume consumer", err));
+      } else if ("stop".equals(action)) {
+        kafkaConsumer.pause(partition)
+          .onFailure(err -> logger.error("Cannot pause consumer", err));
+      }
+    });
+
+  }
+}
