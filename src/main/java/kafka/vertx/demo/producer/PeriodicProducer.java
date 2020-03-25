0748f258430a82e69b97e0de0108e07aaae9cef5
Julien Ponge <julien.ponge@gmail.com>
2020-03-25 11:19:34 +0100

    feat: Fancier start
    * Various Maven POM improvements
    
    - Upgraded versions
    - Use the vertx-dependencies BOM to align dependencies
    
    * Use webSocketHandler instead of websocketHanbler
    
    Deprecated in 3.8.x, removed in v4
    
    * Move to Vertx.next()
    
    * Need jackson-databind as a separate dependency
    
    It is not included in the Vert.x stack by default anymore.
    
    * Fancier and simplified Main / start method
    
    * Replace printf by logger calls
    
    * Kafka is too noisy
    
    Signed-off-by: Julien Ponge <jponge@redhat.com>

--- src/main/java/kafka/vertx/demo/producer/PeriodicProducer.java
+++ src/main/java/kafka/vertx/demo/producer/PeriodicProducer.java
@@ -7,6 +7,8 @@ package kafka.vertx.demo.producer;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,6 +17,8 @@ public class PeriodicProducer extends AbstractVerticle {
     private String topicName;
     private String message;
     private String eventBusId;
+
+    private static final Logger logger = LoggerFactory.getLogger(PeriodicProducer.class);
 
     public PeriodicProducer(String topicName, String message, String eventBusId) {
         this.topicName = topicName;
@@ -31,7 +35,7 @@ public class PeriodicProducer extends AbstractVerticle {
             vertx.eventBus().send(eventBusId, JsonObject.mapFrom(recordData).encode());
             return null;
         }).otherwise(t -> {
-            System.out.printf("Failure in producing record: %s%n", t);
+            logger.error("Failure in producing record", t);
             return null;
         })));
     }
