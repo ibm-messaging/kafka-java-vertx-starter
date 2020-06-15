3b77ce3cde225110816826f92502b29fcf6727ef
Jordan Tucker <jordan.tucker1@ibm.com>
2020-06-15 12:08:03 +0100

    feat: combine bundle and coverage report
    Closes: #71
    
    Signed-off-by: Jordan <jordan.tucker1@ibm.com>

--- .github/actions/bundle/index.js
+++ .github/actions/bundle/index.js
@@ -1,23 +1,16 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
+const core = require("@actions/core");
+const fs = require("fs");
 
 async function calculateBundle() {
   try {
-    const fileSize = Math.round((fs.statSync('./src/main/resources/webroot/main.bundle.js')['size'] / 1024.0) * 100) / 100;
+    const fileSize =
+      Math.round(
+        (fs.statSync("./src/main/resources/webroot/main.bundle.js")["size"] /
+          1024.0) *
+          100
+      ) / 100;
 
-    const github_token = core.getInput('GITHUB_TOKEN');
-
-    const pull_request_number = github.context.payload.pull_request.number;
-    const repo = github.context.repo;
-
-    const octokit = new github.GitHub(github_token);
-    octokit.issues.createComment({
-      ...repo,
-      issue_number: pull_request_number,
-      body: `## Bundle size report\n ${fileSize}KB`
-    });
-
+    core.setOutput("bundle_size", `${fileSize}KB`);
   } catch (error) {
     core.setFailed(error.message);
   }
--- .github/actions/coverage-report/index.js
+++ .github/actions/coverage-report/index.js
@@ -1,38 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
-
-function comment(message) {
-  try {
-    const github_token = core.getInput('GITHUB_TOKEN');
-
-    const pull_request_number = github.context.payload.pull_request.number;
-    const repo = github.context.repo;
-  
-    const octokit = new github.GitHub(github_token);
-    octokit.issues.createComment({
-      ...repo,
-      issue_number: pull_request_number,
-      body: message
-    });
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-}
+const core = require("@actions/core");
+const fs = require("fs");
 
 async function formatCoverage() {
   try {
-    const coverage = JSON.parse(fs.readFileSync("./ui/coverage/jest/coverage-summary.json"));
+    const coverage = JSON.parse(
+      fs.readFileSync("./ui/coverage/jest/coverage-summary.json")
+    );
 
-    let coverageText = "## Test Coverage Report\n| File | Lines | Statement | Functions | Branches |\n| --- | --- | --- | --- | --- |\n";
-  
+    let coverageText =
+      "| File | Lines | Statement | Functions | Branches |\n| --- | --- | --- | --- | --- |\n";
+
     const regex = /^\/(.+\/)*(.+)$/;
-  
-    Object.entries(coverage).forEach(([key,value]) => {
-      coverageText += `| ${key != 'total' ? key.match(regex)[2] : 'Total'} | ${value.lines.pct}% | ${value.statements.pct}% | ${value.functions.pct}% | ${value.branches.pct}% |\n` 
+
+    Object.entries(coverage).forEach(([key, value]) => {
+      coverageText += `| ${key != "total" ? key.match(regex)[2] : "Total"} | ${
+        value.lines.pct
+      }% | ${value.statements.pct}% | ${value.functions.pct}% | ${
+        value.branches.pct
+      }% |\n`;
     });
-  
-    comment(coverageText);
+
+    core.setOutput("test_coverage", coverageText);
   } catch (error) {
     core.setFailed(error.message);
   }
